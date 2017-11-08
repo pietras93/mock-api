2e780ba22ad2f3438098d76e50c6bde2055ef7f9
Piotr Frankowski <Piotr Frankowski>
2017-11-08 16:29:04 +0100

    [Refactor] ES6 ready, moduule wrapped in importable class

--- mock.js
+++ mock.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 'use strict'
 
-//Necessary libs
+// Necessary libs
 const express = require('express')
 const parser  = require('body-parser')
-const app     = express()
 const msg     = require('gulp-messenger')
 const chalk   = require('chalk')
 const fs      = require('fs')
@@ -13,241 +12,292 @@ const cmd     = require('commander')
 const async   = require('async')
 const path    = require('path')
 
-//In-memory database
-var db        = {}
-//Object list
-var objects   = []
-//List of functions that read files
-var fileReadFunctions = []
+class MockApi {
 
-//Arguments parser
-cmd
-  .option('-d, --directory <dir>', '[optional] Path to directory that contains json files, default "db"', 'db')
-  .option('-p, --port <port>', '[optional] Server port, default 3000', (n, d) => +n || d, 3000)
-  .option('-i, --interval <interval>', '[optional] Save to files interval in seconds, default 30', (n, d) => +n || d, 30)
-  .parse(process.argv)
-
-//Seconds to milliseconds
-cmd.interval *= 1000;
-
-//Express config
-app.set('port', (cmd.port || 3000))
-app.use(parser.urlencoded({
-  extended: false
-}))
-app.use(parser.json())
-
-/**
- * Creates express API paths for models
- * @param {object} values - JSON with model contents
- * @param {string} key - model/path name
- */
-function createPaths(values, key) {
-
-  //Get all
-  app.get('/' + key, (req, res) => {
-
-    return res.json(values)
-  })
-
-  //Get element count
-  app.get('/' + key + '/count', (req, res) => {
-
-    return res.json({
-      count: values.length
-    })
-  })
-
-  //Get element with given id
-  app.get('/' + key + '/:id', (req, res) => {
-
-    let requestId = parseInt(req.params.id)
-    let obj = _.find(values, {
-      id: requestId
-    })
-
-    if (!obj) {
-      return res.status(404).json({
-        error: 'Could not find ' + key + ' with id: ' + requestId
-      })
+  constructor(options) {
+    // In-memory database
+    this.db = {}
+    // Routes list
+    this.routes = []
+    // List of functions that read files
+    this.fileReadFunctions = []
+    // File write flag
+    this.busy = false
+    // Express object
+    this.app = express()
+    // Options
+    this.options = {
+      directory: 'db',
+      port: 3000,
+      interval: 30
     }
-
-    return res.json(obj)
-  })
-
-  //Add new element
-  app.post('/' + key, (req, res) => {
-
-    let obj = _.clone(req.body)
-    let last = _.last(_.sortBy(values, 'id'))
-
-    obj.id = last.id + 1
     
-    values.push(obj)
+    Object.assign(this.options, options)
 
-    return res.json(obj)
-  })
+    this.options.interval *= 1000
 
-  //Update element
-  app.put('/' + key + '/:id', (req, res) => {
-
-    let requestId = parseInt(req.params.id)
-    let obj = _.find(values, {
-      id: requestId
-    })
-    
-    if (!obj) {
-      return res.status(404).json({
-        error: 'Could not find ' + key + ' with id: ' + requestId
-      })
-    }
-
-    obj = _.merge(obj, req.body)
-
-    return res.json(obj)
-  })
-
-  //Delete element
-  app.delete('/' + key + '/:id', (req, res) => {
-
-    let requestId = parseInt(req.params.id)
-    let obj = _.clone(_.find(values, {
-      id: requestId
-    }))
-    
-    if (!obj) {
-      return res.status(404).json({
-        error: 'Could not find ' + key + ' with id: ' + requestId
-      })
-    }
-
-    _.remove(values, {
-      id: requestId
-    })
-
-    return res.json(obj)
-  })
-}
-
-/**
- * Saves in-memory data to corresponding files
- * @param {object} values - JSON with model contents
- * @param {string} key - model and file name
- */
-function writeToFile(values, key) {
-
-  return fs.writeFile(path.join(cmd.directory + '/' + key + '.json'), JSON.stringify(values), (err) => {
-
-    if (err) {
-      msg.error('File write error, exiting...')
-      msg.error(err)
-      process.exit(1)
-    }
-  })
-}
-
-/**
- * readFile - fills fileReadFuctions array with function
- * reading file.json from db directory
- * @param {string} file - filename w/o extension
- */
-function readFile(file) {
-
-  var fname = file.split('.') 
-  var fext = fname.pop()
-
-  if (fext === 'json') {
-
-    var f = cb => {
-
-      return fs.readFile(path.join(cmd.directory + '/' + file), 'utf8', (err, data) => {
-          
-        if (err) {
-          return cb(err)
-        }
-
-        if (!data) {
-          msg.error('File: ' + file + ' is empty!');
-          return cb(null)
-        }
-
-        db[fname.join('.')] = JSON.parse(data)
-        objects.push(fname.join('.'))
-
-        return cb(null)
-      });
-    }
-
-    fileReadFunctions.push(f) 
-  }
-}
-
-/**
- * To be called after data is read from files.
- * Calls createPaths for each object and creates file update interval
- * @param {*} err - if exists will be printed in console
- */
-function dbCached(err) {
-
-  if (err) {
-    msg.error('File read error')
-    msg.error(err)
+    this.createPaths = this.createPaths.bind(this)
+    this.writeToFiles = this.writeToFiles.bind(this)
+    this.filesRead = this.filesRead.bind(this)
+    this.readFile = this.readFile.bind(this)
   }
 
-  _.forOwn(db, (values, key) => {
-    
-    createPaths(values, key)
-    setInterval(() => {
-      writeToFile(values, key)
-    }, cmd.interval)
-  })
-}
-
-/**
- * Reads files in db directory and executes fileReadFunctions
- * Exits on dir read error and empty db directory
- */
-function readDirectory() {
-
-  return fs.readdir(cmd.directory, (err, files) => {
-     
-    if (err) {
-      msg.error('Directory read error, exiting...')
-      msg.error(err)
-      process.exit(-1)
-    } 
-
-    if (!files) {
-      msg.error('Empty database directory, exiting...')
-      process.exit(-1)
-    } 
-
-    files.forEach(readFile)
-
-    async.parallel(fileReadFunctions, dbCached)
-  })
-}
-
-// List available paths
-app.get('/', (req, res) => {
+  /**
+   * Creates express API paths for models
+   * @param {object} values - JSON with model contents
+   * @param {string} key - model/path name
+   */
+  createPaths(values, key) {
   
-  return res.json({
-    availableRoutes: objects
-  })
-})
+    // Get all
+    this.app.get('/' + key, (req, res) => {
+  
+      return res.json(values)
+    })
+  
+    // Get element count
+    this.app.get('/' + key + '/count', (req, res) => {
+  
+      return res.json({
+        count: values.length
+      })
+    })
+  
+    // Get element with given id
+    this.app.get('/' + key + '/:id', (req, res) => {
+  
+      let requestId = parseInt(req.params.id)
+      let obj = _.find(values, {
+        id: requestId
+      })
+  
+      if (!obj) {
+        return res.status(404).json({
+          error: 'Could not find ' + key + ' with id: ' + requestId
+        })
+      }
+  
+      return res.json(obj)
+    })
+  
+    // Add new element
+    this.app.post('/' + key, (req, res) => {
+  
+      let obj = _.clone(req.body)
+      let last = _.last(_.sortBy(values, 'id'))
+  
+      obj.id = last.id + 1
+      
+      values.push(obj)
+  
+      return res.json(obj)
+    })
+  
+    // Update element
+    this.app.put('/' + key + '/:id', (req, res) => {
+  
+      let requestId = parseInt(req.params.id)
+      let obj = _.find(values, {
+        id: requestId
+      })
+      
+      if (!obj) {
+        return res.status(404).json({
+          error: 'Could not find ' + key + ' with id: ' + requestId
+        })
+      }
+  
+      obj = _.merge(obj, req.body)
+  
+      return res.json(obj)
+    })
+  
+    // Delete element
+    this.app.delete('/' + key + '/:id', (req, res) => {
+  
+      let requestId = parseInt(req.params.id)
+      let obj = _.clone(_.find(values, {
+        id: requestId
+      }))
+      
+      if (!obj) {
+        return res.status(404).json({
+          error: 'Could not find ' + key + ' with id: ' + requestId
+        })
+      }
+  
+      _.remove(values, {
+        id: requestId
+      })
+  
+      return res.json(obj)
+    })
+  }
+  
+  /**
+   * Saves in-memory data to corresponding files
+   * @param {object} values - JSON with model contents
+   * @param {string} key - model and file name
+   */
+  writeToFiles(values, key) {
+  
+    this.busy = true
+  
+    return fs.writeFile(path.join(this.options.directory + '/' + key + '.json'), JSON.stringify(values), (err) => {
+  
+      if (err) {
+        msg.error('File write error, exiting...')
+        msg.error(err)
+        process.exit(1)
+      }
+  
+      this.busy = false
+    })
+  }
 
-app.listen(app.get('port'), () => {
+  /**
+   * To be called after data is read from files.
+   * Calls createPaths for each object and creates file update interval
+   * @param {*} err - if exists will be printed in console
+   */
+  filesRead(err) {
+  
+    if (err) {
+      msg.error('File read error')
+      msg.error(err)
+    }
+  
+    _.forOwn(this.db, (values, key) => {
+      
+      // Create express routes once
+      this.createPaths(values, key)
+      this.busy = false
 
-  const serverInfo = chalk.yellow('http://localhost:' + app.get('port'))
+      // Write memory db to files every INTERVAL
+      setInterval(() => {
+        this.writeToFiles(values, key)
+      }, this.options.interval)
+    })
+  }
 
-  msg.log('\n')
-  msg.log(chalk.cyan('Reading directory: "' + cmd.directory + '"'))
-  msg.log(chalk.cyan('File save interval: ' + (cmd.interval / 1000) + ' ms'))
-  msg.log(chalk.cyan('Mock API Running On: "' + serverInfo + '"'))
-  msg.log('\n')
+  /**
+   * readFile - fills fileReadFuctions array with function
+   * reading file.json from db directory
+   * @param {string} file - filename w/o extension
+   */
+  readFile(file) {
+    
+    let fname = file.split('.') 
+    let fext = fname.pop()
+  
+    if (fext === 'json') {
+  
+      let f = (cb) => {
+  
+        return fs.readFile(path.join(this.options.directory + '/' + file), 'utf8', (err, data) => {
+            
+          if (err) {
+            return cb(err)
+          }
+  
+          if (!data) {
+            msg.error('File: ' + file + ' is empty!');
+            return cb(null)
+          }
+  
+          this.db[fname.join('.')] = JSON.parse(data)
+          this.routes.push(fname.join('.'))
+  
+          return cb(null)
+        })
+      }
 
-  readDirectory()
+      // Create array of file readding functions
+      this.fileReadFunctions.push(f) 
+    }
+  }
 
-  msg.log(chalk.green('Mock API Started ' + new Date()))
-  msg.log('\n')
-})
+  /**
+   * Reads files in db directory and executes fileReadFunctions
+   * Exits on dir read error and empty db directory
+   */
+  init() {
+    
+    this.busy = true
+
+    return fs.readdir(this.options.directory, (err, files) => {
+        
+      if (err) {
+        msg.error('Directory read error, exiting...')
+        msg.error(err)
+        process.exit(-1)
+      } 
+
+      if (!files) {
+        msg.error('Empty database directory, exiting...')
+        process.exit(-1)
+      } 
+
+      files.forEach(this.readFile)
+
+      async.parallel(this.fileReadFunctions, this.filesRead)
+    })
+  }
+
+  start() {
+    
+    // Express config
+    this.app.set('port', this.options.port)
+    this.app.use(parser.urlencoded({
+      extended: false
+    }))
+    this.app.use(parser.json())
+
+    // Break if busy
+    this.app.use((req, res, next) => {
+    
+      if (this.busy) {
+        return res.status(400).send({ message: 'Server is busy at the moment, please try again in a moment' })
+      }
+    
+      return next()
+    })
+    
+    // List available paths
+    this.app.get('/', (req, res) => {
+      
+      return res.json({
+        availableRoutes: this.routes
+      })
+    })
+
+    // Start listening
+    this.app.listen(this.app.get('port'), () => {
+      
+        const serverInfo = chalk.yellow('http://localhost:' + this.app.get('port'))
+      
+        msg.log('\n')
+        msg.log(chalk.cyan('Reading directory: "' + this.options.directory + '"'))
+        msg.log(chalk.cyan('File save interval: ' + (this.options.interval / 1000) + ' s'))
+        msg.log(chalk.cyan('Mock API Running On: "' + serverInfo + '"'))
+        msg.log('\n')
+      
+        this.init()
+      
+        msg.log(chalk.green('Mock API Started ' + new Date()))
+        msg.log('\n')
+      })
+  }
+}
+
+module.exports = MockApi
+
+// Arguments parser
+cmd
+.option('-d, --directory <dir>', '[optional] Path to directory that contains json files, default "db"', 'db')
+.option('-p, --port <port>', '[optional] Server port, default 3000', (n, d) => +n || d, 3000)
+.option('-i, --interval <interval>', '[optional] Save to files interval in seconds, default 30', (n, d) => +n || d, 30)
+.parse(process.argv)
+
+let mock = new MockApi(cmd)
+mock.start()
